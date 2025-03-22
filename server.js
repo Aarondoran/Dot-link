e3c08779f3b192165ba63920842c056b63b41264
Aaron doran <118635428+Aarondoran@users.noreply.github.com>
2025-03-22 22:48:15 +0000

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const { Pool } = require("pg");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 // ✅ PostgreSQL connection setup
 const pool = new Pool({
